f866befd07fe6acb4402f4fd26a2f222600111d1
Rafaela Machado <rafaelamachado2008@gmail.com>
2025-12-28 14:10:54 -0300

    Implement login tests in Cypress
    Add Cypress tests for login functionality with valid credentials.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,1 +1,16 @@
+describe('Login – Plataforma IFVEST', () => {
+
+  beforeEach(() => {
+    cy.visit('/login')
+  })
+
+  it('Deve realizar login com credenciais válidas', () => {
+    cy.get('[name="usuario"]').type('usuario_teste')
+    cy.get('[name="senha"]').type('senha_teste')
+    cy.get('.btn-login').click()
+
+    // Validação simples pós-login
+    cy.url().should('not.include', '/login')
+  })
+})
 
